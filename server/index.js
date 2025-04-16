b8006d44d6687636eec310a3daf91491cd4a715f
Marc Grupe <marc.grupe@gmail.com>
2025-04-16 15:51:41 -0400

    initial upload

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
 // imports here for express and pg
+require("dotenv").config();
+import express from 'express';
+const app = express();
+import pg from 'pg';
+const PORT = 3000;
+console.log(process.env.DATABASE_URL);
+const client = new pg.Client(process.env.DATABASE_URL);
+import cors from 'cors';
+
+
+
+app.use(cors());
 
 // static routes here (you only need these for deployment)
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
 // app routes here
+// app.get('/api/employees', (req, res) => {
+//   const allEmployees = await client.query(
+//     SELECT * FROM acme_hr_db
+//   )
+// });
 
 // create your init function
 
